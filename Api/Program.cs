132fa66cd40e327bb60377f7dfb7c7c6613945fd
davidleonmayor <jd.leon@unimayor.edu.co>
2026-03-10 17:40:44 -0500

    feat(): adding Logger

--- Api/Program.cs
+++ Api/Program.cs
@@ -3,8 +3,20 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using Microsoft.OpenApi;
+using Serilog;
+using Serilog.Events;
 
 var builder = WebApplication.CreateBuilder(args);
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Host.UseSerilog((ctx, services, cfg) =>
+    {
+        cfg.ReadFrom.Configuration(ctx.Configuration)
+           .ReadFrom.Services(services)
+           .Enrich.FromLogContext();
+    });
+}
 
 // Database
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -67,6 +79,39 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    app.UseSerilogRequestLogging(options =>
+    {
+        options.MessageTemplate =
+            "HTTP {RequestMethod} {RequestPath} responded {StatusCode} in {Elapsed:0.0000} ms";
+
+        options.GetLevel = (httpContext, elapsed, ex) =>
+        {
+            if (ex is not null)
+                return LogEventLevel.Error;
+
+            var status = httpContext.Response.StatusCode;
+            return status switch
+            {
+                >= 500 => LogEventLevel.Error,
+                >= 400 => LogEventLevel.Warning,
+                _ => LogEventLevel.Information
+            };
+        };
+
+        options.EnrichDiagnosticContext = (diagnosticContext, httpContext) =>
+        {
+            diagnosticContext.Set("TraceId", httpContext.TraceIdentifier);
+
+            var user = httpContext.User?.Identity;
+            if (user?.IsAuthenticated == true && !string.IsNullOrWhiteSpace(user.Name))
+                diagnosticContext.Set("User", user.Name);
+
+            var ip = httpContext.Connection.RemoteIpAddress?.ToString();
+            if (!string.IsNullOrWhiteSpace(ip))
+                diagnosticContext.Set("RemoteIP", ip);
+        };
+    });
 }
 
 app.UseMiddleware<ExceptionHandlingMiddleware>();
