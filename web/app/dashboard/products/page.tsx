353158bb0d32e601ae2f255512e6afcab5425b9f
davidleonmayor <jd.leon@unimayor.edu.co>
2026-05-11 19:21:09 -0500

    feat(UI): manipulate products cards - page to add new product / - page to update or change spesific product - page to show all products

--- web/app/dashboard/products/page.tsx
+++ web/app/dashboard/products/page.tsx
@@ -1,9 +1,4 @@
-import {
-  Tabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger,
-} from "@/components/ui/tabs";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 import { ProductUploadForm } from "@/components/products/product-upload-form";
 import { ProductsList } from "@/components/products/products-list";
